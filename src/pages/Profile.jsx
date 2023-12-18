f0a5d7994e66bc94337b399e5d680af5a110d1e5
pratikash <pratikashpanda.1234@gmail.com>
2023-12-18 12:06:59 +0530

    complete delete user functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -13,6 +13,9 @@ import {
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -87,6 +90,24 @@ const Profile = () => {
     }
   };
 
+  const handleDeleteUser = async () =>{
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`api/user/delete/${currentUser._id}`, {
+        method : 'DELETE',
+      })
+      const data = await res.json;
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.apply.message));
+        return;
+      }
+
+      dispatch(deleteUserSuccess())
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
   return (
     <div className="p-3 mx-auto max-w-lg">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -143,7 +164,7 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5 uppercase">{error ? error : ''}</p>
